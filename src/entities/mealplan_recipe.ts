5c8dcfc1fad9c2e0aa70d7dfec09a6a854f0a711
João Wasquevite <jvwasquevite@gmail.com>
2024-03-03 06:48:48 -0300

    fix: refactored entities for mathing the new database version

---
+++ src/entities/mealplan_recipe.ts
@@ -0,0 +1,40 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm"
+import { v4 as uuid } from "uuid"
+import { Recipe } from "./recipe"
+import { MealPlan } from "./mealplan"
+
+enum Weekdays {
+  Segunda = "Segunda",
+  Terça = "Terça",
+  Quarta = "Quarta",
+  Quinta = "Quinta",
+  Sexta = "Sexta",
+  Sábado = "Sábado",
+  Domingo = "Domingo",
+}
+
+@Entity("mealplan_recipe")
+class MealPlanRecipe {
+  @PrimaryColumn()
+  readonly id: string
+
+  @Column({ type: "enum", enum: Weekdays })
+  weekday: Weekdays
+
+  @Column()
+  mealblock: string
+
+  @ManyToOne(type => Recipe, recipe => recipe.mealplans)
+  recipe: Recipe
+
+  @ManyToOne(type => MealPlan, mealPlan => mealPlan.recipes)
+  mealplan: MealPlan
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid()
+    }
+  }
+}
+
+export { MealPlanRecipe }
