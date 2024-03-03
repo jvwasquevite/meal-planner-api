88fc95e50c2cd3c1f6cb98512de24b9fe71f9ebf
João Wasquevite <jvwasquevite@gmail.com>
2024-03-03 08:09:54 -0300

    refactor: modified listRecipesUseCase to show ingredients and instructions minified

--- src/useCases/listRecipesUseCase.ts
+++ src/useCases/listRecipesUseCase.ts
@@ -5,10 +5,36 @@ export class listRecipesUseCase {
   async execute() {
     const repo = Database.getRepository(Recipe)
 
-    const recipes = await repo.find({
-      relations: ["ingredients", "instructions", "mealplans"],
-    })
+    const recipes = await repo
+      .createQueryBuilder("recipe")
+      .leftJoinAndSelect("recipe.ingredients", "recipe_ingredient")
+      .leftJoinAndSelect("recipe_ingredient.ingredient", "ingredient")
+      .leftJoinAndSelect("recipe.instructions", "instruction")
+      .getMany()
 
-    return recipes
+    // Convert recipes to JSON with ingredients and instructions minified for each recipe
+    const recipesWithIngredientsAndInstructionsMinified = recipes.map(
+      recipe => {
+        const ingredients = recipe.ingredients.map(recipeIngredient => {
+          return {
+            id: recipeIngredient.ingredient.id,
+            name: recipeIngredient.ingredient.name,
+            quantity: recipeIngredient.quantity,
+          }
+        })
+
+        const instructions = recipe.instructions.map(instruction => {
+          return instruction.name
+        })
+
+        return {
+          ...recipe,
+          ingredients: ingredients,
+          instructions: instructions,
+        }
+      }
+    )
+
+    return recipesWithIngredientsAndInstructionsMinified
   }
 }
