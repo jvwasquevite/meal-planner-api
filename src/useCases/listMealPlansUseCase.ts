6944141dd7947d607ea204ed246c406ede2321c0
João Wasquevite <jvwasquevite@gmail.com>
2024-03-06 10:09:25 -0300

    refactor: return list of recipes and formated created_at on list meal plans

--- src/useCases/listMealPlansUseCase.ts
+++ src/useCases/listMealPlansUseCase.ts
@@ -1,12 +1,45 @@
 import { Database } from "../database"
 import { MealPlan } from "../entities/mealplan"
+import { MealPlanRecipe } from "../entities/mealplan_recipe"
 
 export class listMealPlansUseCase {
   async execute() {
     const repo = Database.getRepository(MealPlan)
+    const mealPlanRecipeRepo = Database.getRepository(MealPlanRecipe)
 
     const mealPlans = await repo.find()
 
-    return mealPlans
+    const mealPlanData = []
+
+    for (const mealPlan of mealPlans) {
+      const mealPlanItem = {
+        id: mealPlan.id,
+        name: mealPlan.name,
+        created_at: mealPlan.created_at.toLocaleString("pt-BR", {
+          month: "long",
+          year: "numeric",
+        }),
+        recipes: [],
+      }
+
+      const mealPlanRecipes = await mealPlanRecipeRepo
+        .createQueryBuilder("mealplan_recipe")
+        .leftJoinAndSelect("mealplan_recipe.recipe", "recipe")
+        .where("mealplan_recipe.mealplanId = :mealPlanId", {
+          mealPlanId: mealPlan.id,
+        })
+        .getMany()
+
+      mealPlanRecipes.forEach(mealPlanRecipe => {
+        mealPlanItem.recipes.push({
+          id: mealPlanRecipe.recipe.id,
+          name: mealPlanRecipe.recipe.name,
+        })
+      })
+
+      mealPlanData.push(mealPlanItem)
+    }
+
+    return mealPlanData
   }
 }
