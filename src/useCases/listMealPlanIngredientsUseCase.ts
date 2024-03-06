d7e5c1b4024599e70b84e9e01a3c9eee607afb3f
João Wasquevite <jvwasquevite@gmail.com>
2024-03-06 17:18:43 -0300

    refactor: changed meal plan ingredients to return formatted quantity

--- src/useCases/listMealPlanIngredientsUseCase.ts
+++ src/useCases/listMealPlanIngredientsUseCase.ts
@@ -15,6 +15,7 @@ export class listMealPlanIngredientsUseCase {
       .leftJoinAndSelect("mealplanRecipe.recipe", "recipe")
       .leftJoinAndSelect("recipe.ingredients", "ingredients")
       .leftJoinAndSelect("ingredients.ingredient", "ingredient")
+      .leftJoinAndSelect("ingredient.units", "units")
       .where("mealplan.id = :id", { id: mealplan_id })
       .getOne()
 
@@ -25,18 +26,40 @@ export class listMealPlanIngredientsUseCase {
         const existingIngredient = mealPlanIngredients.find(
           ingredient => ingredient.id === recipeIngredient.ingredient.id
         )
+
         if (!existingIngredient) {
           mealPlanIngredients.push({
             id: recipeIngredient.ingredient.id,
             name: recipeIngredient.ingredient.name,
+            consumption_unit:
+              recipeIngredient.ingredient.units.consumption_unit,
             quantity: recipeIngredient.converted_quantity,
           })
         } else {
           existingIngredient.quantity += recipeIngredient.converted_quantity
         }
+
+        mealPlanIngredients.forEach(
+          async ingredient =>
+            (ingredient["formatted_quantity"] = await this.format_quantity(
+              ingredient.quantity,
+              ingredient.consumption_unit
+            ))
+        )
       })
     })
 
     return mealPlanIngredients
+  }
+
+  format_quantity(quantity, consumption_unit) {
+    const consumption_units = {
+      "Peso sólido": "g",
+      "Peso líquido": "ml",
+      Unidade: quantity > 1 ? " unidades" : " unidade",
+      Fatia: quantity > 1 ? " fatias" : " fatia",
+    }
+
+    return quantity + consumption_units[consumption_unit]
   }
 }
